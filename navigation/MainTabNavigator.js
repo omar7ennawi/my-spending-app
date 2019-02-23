1955747eb01d5bd7b92dd1b7195dc82ca98371e1
Omar <omar.elhennawi97@gmail.com>
2019-02-23 12:17:06 +0800

    React Native

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Platform } from 'react-native';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
-
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
-
 const HomeStack = createStackNavigator({
   Home: HomeScreen,
 });
-
 HomeStack.navigationOptions = {
   tabBarLabel: 'Home',
   tabBarIcon: ({ focused }) => (
@@ -18,41 +15,36 @@ HomeStack.navigationOptions = {
       focused={focused}
       name={
         Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
+          ? `ios-home`
+          : 'md-home'
       }
     />
   ),
 };
-
 const LinksStack = createStackNavigator({
   Links: LinksScreen,
 });
-
 LinksStack.navigationOptions = {
   tabBarLabel: 'Links',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
+      name={Platform.OS === 'ios' ? 'ios-add' : 'md-add'}
     />
   ),
 };
-
 const SettingsStack = createStackNavigator({
   Settings: SettingsScreen,
 });
-
 SettingsStack.navigationOptions = {
   tabBarLabel: 'Settings',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+      name={Platform.OS === 'ios' ? 'ios-settings' : 'md-settings'}
     />
   ),
 };
-
 export default createBottomTabNavigator({
   HomeStack,
   LinksStack,
--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,19 +1,75 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  Platform,
+  View,
+  TouchableOpacity,
+  DatePickerIOS,
+  DatePickerAndroid
+} from "react-native";import { TextInput } from 'react-native-gesture-handler';
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links',
+    title: 'Add Item',
+    width: 20
   };
+
+  state = { amount: '', desc: '', date: new Date() }
 
   render() {
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
+      <View style={styles.container}>
+        <ScrollView>
+          <View style={styles.row}>
+            <Text style={{ fontSize: 18 }}>Price</Text>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Text>RM </Text>
+              <TextInput
+                style={styles.textInput}
+                onChangeText={(input) => this.setState({ amount: input })}
+                value={this.state.text}
+                keyboardType='numeric'
+              />
+            </View>
+          </View>
+          <View style={styles.row}>
+             <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+               <TextInput
+                 style={[styles.textInput, {flex: 1}]}
+                 onChangeText={(desc) => this.setState({ desc })}
+                 value={this.state.text}
+                 keyboardType='numeric'
+                 placeholder="Description"
+               />
+             </View>
+           </View>
+           <View style={styles.row}>
+             <View style={{ flex: 1 }}>
+               {
+                 Platform.OS === 'ios' ?
+                   <DatePickerIOS
+                     date={this.state.date}
+                     onDateChange={(date) => this.setState({ date })}
+                   /> :
+                   <TouchableOpacity
+                     onPress={async () => {
+                        const {year, month, day} = await DatePickerAndroid.open({
+                          date: new Date()
+                        });
+                        this.setState({date: new Date(year, month, day)})
+                     }}>
+                     <Text>{this.state.date.toString()}</Text>
+                   </TouchableOpacity>
+               }
+             </View>
+           </View>
+        </ScrollView>
+        <TouchableOpacity onPress={() => alert(JSON.stringify(this.state))} style={styles.tabBarStickyBottom}>
+            <Text style={{ fontWeight: 'bold' }}>Add </Text>
+          </TouchableOpacity>
+      </View>
     );
   }
 }
@@ -21,7 +77,44 @@ export default class LinksScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
     backgroundColor: '#fff',
   },
-});
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    paddingVertical: 30,
+    borderWidth: 0.5,
+    borderColor: '#d6d7da',
+  },
+  textInput: {
+    width: 80,
+    borderWidth: 0.5,
+    borderColor: '#d6d7da',
+    padding: 10
+  },
+  tabBarStickyBottom: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    alignItems: 'center',
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 20,
+    flexDirection: 'row',
+    justifyContent: 'center',    
+    padding: 10,
+  }
+}); 
